3b17d38b2b08785bce555512e24e04a55e772ea9
suiyan-li@pub <lisy09.thu@gmail.com>
2020-12-24 18:46:30 +0900

    Update livy-entrypoint.sh

--- livy_docker/livy-entrypoint.sh
+++ livy_docker/livy-entrypoint.sh
@@ -5,6 +5,6 @@
 
 mkdir -p $HADOOP_CONF_DIR
 cp $HADOOP_CONF_DIR_ORIGIN/* $HADOOP_CONF_DIR/
-xmlstarlet ed -u /configuration/property/yarn.timeline-service.enabled -v false $HADOOP_CONF_DIR/yarn-site.xml
+xmlstarlet ed -u "//configuration/property/name[contains(text(),yarn.timeline-service.enabled)]/following-sibling::value" -v false $HADOOP_CONF_DIR/yarn-site.xml
 
 /usr/local/livy/bin/livy-server
