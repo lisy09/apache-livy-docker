90bd3a282d855ee240f7b01e9a9ece925cbeab6c
suiyan-li@pub <lisy09.thu@gmail.com>
2020-12-24 18:15:12 +0900

    Update livy-entrypoint.sh

--- livy_docker/livy-entrypoint.sh
+++ livy_docker/livy-entrypoint.sh
@@ -5,6 +5,6 @@
 
 mkdir -p $HADOOP_CONF_DIR
 cp $HADOOP_CONF_DIR_ORIGIN/* $HADOOP_CONF_DIR/
-xmlstarlet ed -u /configuration/property/yarn.timeline-service.enabled -v false $HADOOP_CONF_DIR/yarn-sites.xml
+xmlstarlet ed -u /configuration/property/yarn.timeline-service.enabled -v false $HADOOP_CONF_DIR/yarn-site.xml
 
 /usr/local/livy/bin/livy-server
